633f9379417ab2ec9de53b1b35a2b45c850daf2b
johnnyx <john.xavier.vera@outlook.com>
2018-12-12 17:52:31 +0000

    01-step1-setup-project

---
+++ src/main/java/com/postgresnosql/nosql/PostgresNoSqlApp.java
@@ -0,0 +1,11 @@
+package com.postgresnosql.nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresNoSqlApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresNoSqlApp.class, args);
+    }
+}
---
+++ src/main/resources/db/sql/dbCreation.sql
@@ -0,0 +1,29 @@
+--create the DB
+set search_path to postgres_no_sql;
+
+
+create table if not exists users_sql
+(
+  id         serial8 primary key,
+  first_name varchar(30),
+  last_name  varchar(50),
+  email      varchar(50),
+  address_id integer
+);
+
+create table if not exists users_doc
+(
+  id            serial8 primary key,
+  user_document jsonb
+);
+
+create table if not exists address
+(
+  id            serial primary key,
+  user_id       integer references users_sql (id),
+  street_name   varchar(50),
+  street_number varchar(7),
+  city          integer,
+  pos_code      varchar(10)
+
+)
